405092989d9a2444c75e6c22272f1923bcdf4291
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2011-11-16 14:15:58 +0100

    roby: set the role of frame producer tasks to reflect their usage as transformer producers

--- rock/roby_plugin.rb
+++ rock/roby_plugin.rb
@@ -1111,7 +1111,7 @@ module Transformer
                     end
                     producer_task.select_port_for_transform(out_port, dyn.from, dyn.to)
                     producer_task.connect_ports(task, [out_port.name, "dynamic_transformations"] => Hash.new)
-                    task.depends_on(producer_task)
+                    task.depends_on(producer_task, :role => "transformer_#{dyn.from}2#{dyn.to}")
                     task.should_start_after producer_task
                 end
             end
